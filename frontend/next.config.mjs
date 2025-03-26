11d4423aefed4a9311f2627c275bc721d24d74af
HaidarSh <haidarshokor2@gmail.com>
2025-03-27 00:30:26 +0200

    xd

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: "/api/:path*",
-          destination: "http://127.0.0.1:8000/api/:path*",
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sales-management-system-backend-2.onrender.com/api/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
